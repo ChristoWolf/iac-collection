7d16d8b3eba24c464d367915edeac1bdf95ad62f
ChristoWolf <christoph.n.wolf@gmail.com>
2021-07-23 19:19:04 +0200

    This commit is exactly what I deserve for copy and pasting...
    Related: #2

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -2,6 +2,6 @@
 
 # See https://github.com/golang/go/wiki/Ubuntu.
 # Official way would be https://golang.org/doc/install.
-$ sudo add-apt-repository -y ppa:longsleep/golang-backports
-$ sudo apt update
-$ sudo apt install golang-go
+sudo add-apt-repository -y ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install golang-go
