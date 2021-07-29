9b2d3feb7961b264f459f9466fc66b634638a334
ChristoWolf <christoph.n.wolf@gmail.com>
2021-07-29 19:04:06 +0200

    Fixed issue with guest additions not being complete (which resulted in shared folder not being mountable)
    Related: #5

--- scripts/install-guestadditions.sh
+++ scripts/install-guestadditions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Based on https://askubuntu.com/a/193632.
+# Somehow needed: https://askubuntu.com/a/1196701.
+sudo apt-get install -y dkms
 
+# Based on https://askubuntu.com/a/193632.
 ISO_PATH=/home/vagrant/VBoxGuestAdditions.iso
 TMP_MOUNT=/tmp/iso
-
 mkdir $TMP_MOUNT
 mount -t iso9660 -o loop $ISO_PATH $TMP_MOUNT
 $TMP_MOUNT/VBoxLinuxAdditions.run --nox11
