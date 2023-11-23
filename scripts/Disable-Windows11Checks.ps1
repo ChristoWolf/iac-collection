7af9aa1974fedf7b3a859c7560776cf9b1d17706
ChristoWolf <christoph.n.wolf@gmail.com>
2023-11-23 17:24:20 +0100

    feat: implemented and referenced Disable-Windows11Checks.ps1

--- scripts/Disable-Windows11Checks.ps1
+++ scripts/Disable-Windows11Checks.ps1
@@ -2,6 +2,16 @@
 # see https://github.com/StefanScherer/packer-windows/blob/main/windows_11.json
 # and https://gearupwindows.com/fix-this-pc-cant-run-windows-11-error-in-virtualbox-or-vmware/.
 
-# TODO: As PowerShell cmdlets (and other stuff inhttps://gearupwindows.com/fix-this-pc-cant-run-windows-11-error-in-virtualbox-or-vmware/)!
-reg add HKLM\\SYSTEM\\Setup\\LabConfig /t REG_DWORD /v BypassTPMCheck /d 1
-reg add HKLM\\SYSTEM\\Setup\\LabConfig /t REG_DWORD /v BypassSecureBootCheck /d 1
+# Always disable progress output,
+# see https://github.com/crc-org/crc/issues/1184#issuecomment-1182124711.
+# It's in any case redundant, 
+# as its not piped to the output stream.
+$ProgressPreference = "SilentlyContinue"
+
+$regPath = "HKLM:\SYSTEM\Setup\LabConfig"
+$regNames = @("BypassTPMCheck","BypassSecureBootCheck","BypassRAMCheck","BypassCPUCheck")
+$regValue = 1
+New-Item -Path $regPath -Force -ErrorAction "SilentlyContinue"
+foreach ($regName in $regNames) {
+    Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type DWORD -Force
+}
