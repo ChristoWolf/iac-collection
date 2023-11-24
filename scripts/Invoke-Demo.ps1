e7e39c5fb4f94bb55a482fa7e1503288e2a5371b
Christoph Wolf <christoph.wolf@medel.com>
2023-11-24 12:32:12 +0100

    feat, fix: integrated demo now s.t. it can simply be called from inside the VM, simulating agent behavior

--- scripts/Invoke-Demo.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process "C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe"
-Start-Sleep -Seconds 5
-& C:/Demo.exe
--- tools/Demo/Program.cs
+++ tools/Demo/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 
+var process = Process.Start(@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
 var options = new AppiumOptions();
 options.AddAdditionalCapability("platformName", "Windows");
 options.AddAdditionalCapability("platformVersion", "10");
 options.AddAdditionalCapability("deviceName", "WindowsPC");
-options.AddAdditionalCapability("ms:waitForAppLaunch", 5);
+options.AddAdditionalCapability("ms:waitForAppLaunch", 30);
 // See https://github.com/microsoft/WinAppDriver/blob/master/Samples/C%23/CalculatorTest/CalculatorSession.cs.
 options.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
-var driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), options, TimeSpan.FromSeconds(10));
+var driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), options, TimeSpan.FromMinutes(1));
 var calc = driver.FindElementByName("Calculator");
 var buttonOne = calc.FindElementByAccessibilityId("num1Button");
 var buttonPlus = calc.FindElementByAccessibilityId("plusButton");
@@ -23,3 +25,4 @@ buttonEquals.Click();
 var result = Regex.Match(resultDisplay.Text, @"\d+").Value;
 Console.WriteLine($"1 + 1 = {result}");
 driver.Quit();
+process.Kill();
